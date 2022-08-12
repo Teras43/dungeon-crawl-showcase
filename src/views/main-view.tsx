e0aacb8fbf5f120e754f1906e8f830c9a3e6ea1e
Brandon Curtis <Brandcurtis43@gmail.com>
2022-08-11 21:06:12 -0600

    Added initial component files and baseUrl in tsconfig. Added index files for views and component folders.

---
+++ src/views/main-view.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const MainView = () => {
+  return (
+    <PageWrap>
+      <div></div>
+    </PageWrap>
+  );
+};
+
+// Styles
+const PageWrap = styled.div``;
+
+// Exports
+export default MainView;
